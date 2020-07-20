5ec1fea3999f1285d32b8c814268d797183c0bc3
Preston Thomsen <preston.thomsen@esmsnyc.net>
2020-07-20 18:38:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+};
+function nestedTarget() {
+  return #nested.querySelector(.target)
+};
+function increaseRankBy(n) {
+  var i = 0
+  var john = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < john.length; i++) {
+  john[i].innerHTML = (john[i] + n).toString();
+  }
+};
+function deepestChild() {
+  const lis = document.getElementById('app').querySelectorAll('div.grand-node div');
+  const lis2
+  for (let i = 0; i < lis.length; i++) {
+    if (lis2.querySelectorAll('div.grand-node div') == null) {
+      i = 1
+      return lis2
+    } else {
+      lis2 = lis2.querySelectorAll('div.grand-node div')
+    }
+  }
+};
